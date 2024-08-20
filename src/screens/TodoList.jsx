ef6a1269db4ce9c0728e7946f0921e6d71e2159f
Netik Kohli <netikkohli9@gmail.com>
2024-08-20 16:14:51 +0530

    Improve UI

--- src/screens/TodoList.jsx
+++ src/screens/TodoList.jsx
@@ -1,35 +1,49 @@
 import React, { useState } from 'react'
 import './TodoListCss.css'
+import { MdDeleteSweep } from "react-icons/md";
 function TodoList() {
-  const [list,setList]=useState([])
-  const [item,setItem]=useState("")
-  const addItem=()=>{
-    const newList={id:Math.random(), todo:item};
-    setList([...list,newList]);
-    console.log(list)
-    setItem("");
+  const [list, setList] = useState([])
+  const [item, setItem] = useState("")
+  let newList = []
+  const addItem = () => {
+    {
+      if (item !== "") {
+        newList = { id: Math.random(), todo: item }
+        setList([...list, newList]);
+        console.log(list)
+        setItem("")
+      }
+    }
   }
-  function deleteItem(idGiven){
-    const newlist=list.filter((todo)=>todo.id!==idGiven)
+
+
+  function deleteItem(idGiven) {
+    const newlist = list.filter((todo) => todo.id !== idGiven)
     setList(newlist)
   }
   return (
     <div className='main'>
       <div className='wrapper'>
-      <h1 className='h'>TODO List React App</h1>
-       <input class='input' type="text" value={item} onChange={(e)=>{setItem(e.target.value)}}/>
-       <button id='addButton' onClick={addItem}>Add Todo</button>
-      </div> 
-    <ul>
-      {list.map(({id,todo})=>(
-         <li key={id}>
-             {todo} &nbsp;
-             <button onClick={()=>deleteItem(id)}>&times;</button>
-         </li>
-      ))}
-    </ul>
+        <h1 className='h'>TODO List React App</h1>
+        <div className='todoadd'>
+          <input class='input' type="text" value={item} onChange={(e) => { setItem(e.target.value) }} placeholder='What is your target?' />
+          <button id='addButton' onClick={addItem}>Add Todo</button>
+        </div>
+         
+          {list.map(({ id, todo }) => (
+            <div className="todoParent" key={id}>
+              <div className='todo'>{todo}</div>
+              <button onClick={()=>deleteItem(id)}><MdDeleteSweep /></button>
+              
+            </div>
+          ))}
+      
+
+      </div>
+
     </div>
   )
 }
+
 
 export default TodoList
